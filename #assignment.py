8800c2f5d336b9451caff68c7d089cf0474c992f
Intellectualdeveloper <pranveergehlot2006@gmail.com>
2026-06-09 16:11:05 +0530

    first commit

---
+++ #assignment.py
@@ -0,0 +1,118 @@
+#assignment
+#write a program to print your name,age and course.all the details should be stored in seperate variables and print
+
+name = "Pranveer"
+age = 20
+course ="B.Tech CSE"
+
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
+
+#write a program to print the name ,age and course details of three students.all the details should be stored in seperate variables and then print
+# use \n for gaps
+
+name1 = "Pranveer"
+age1 = "20"
+course1 = "B.tech CSE"
+
+name2 = "Jatin"
+age2 = "21"
+course2 = "Btech Electrical"
+
+name3 = "Himanshu"
+age3 = "19"
+course3 = "B.Tech Cse"
+
+#to specify students we add student 1,2,3
+
+print("Student 1")
+print("Name:",name1)
+print("Age:",age1)
+print("Course:",course1)
+
+print("\nStudent 2")
+print("Name:",name2)
+print("Age:",age2)
+print("Course:",course2)
+
+print("\nStudent 3")
+print("Name:",name3)
+print("Age:",age3)
+print("Course:",course3)
+
+#Write a program to sum 2 integer variable values without using a third variables
+
+a = 2
+b = 3
+
+print(a + b)
+
+#write a program to multiply 2 floating points
+
+a = 2.5
+b = 3.5
+
+print(a*b)
+
+#write a program to print the ASCII Value of a character variable
+
+ch = input("Enter a character")
+if len(ch)==1:
+    print("ascii_value of Ch is",{ord(ch)})
+else:
+    print("Error")
+
+
+
+
+#Write a program to create 2 variables,initialize them with integer values,print the sum of both variables
+
+# Here we take variable as a object A and B, Initalize is to store the value in this variable
+
+A = 4
+B = 3
+
+print(A + B)
+
+#Write a program to create 2 variable,initialize them with integer values,print the difference of both variables
+
+A = 3
+B = 2
+
+print(A-B)
+
+#Write a program to create 2 variables,initialize them with integer values,print the multiplication of both variables
+
+A = 3
+B = 2
+
+print(A*B)
+
+#Write a program to create 2 variables,initialize them with integer values,Print the division of both variables
+
+A = 6
+B = 2
+
+print(A/B)
+
+#Write a program to create 2 variables,initialize them with integer values,Print the division floored values of both variables
+
+A = 6
+B = 4
+
+print(A//B)
+
+#Write a program to create 2 variables,initialize them with integer values,Print the remainder after the division of both variables
+
+A = 7
+B = 4
+
+print(A%B)
+
+#Write a program to create 2 variables,initialize them with integer values,Print the value which is first variable to the power of second variable
+
+A = 2
+B = 8
+
+print(A**B)
---
+++ Pranveer.py
@@ -0,0 +1,29 @@
+data_link= "star"
+
+
+print(data_link)
+print("best",data_link)
+a_player = True
+if a_player:
+    print("needed a player")
+else:
+    print("needed NOT a player")    
+name = "bot"
+age = 19
+kd = 5.5
+a_legend = True
+#typecasting = the process of converting a variable from one data type to another 
+# str(),int(),float(),bool()
+print(type(kd))
+kd = int(kd)
+print(kd)
+age = float(age)
+print(age)
+name += "1"
+print(name)
+
+# input() = A function that prompts the user to enter data returns the entered data as a string
+activity = input("konsa course kar rahe ho ")
+
+print (f"OH I SEE {activity} that is a very nice course")
+
